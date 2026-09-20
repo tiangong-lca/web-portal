be2ec89921a5af8c7db9f9b3c4082cfe97b78913
nanli <linanenv@gmail.com>
2026-09-21 02:07:14 +0800

    docs: publish real imports for Collapsible story metadata

--- src/components/ui/collapsible.tsx
+++ src/components/ui/collapsible.tsx
@@ -2,16 +2,19 @@
 
 import { Collapsible as CollapsiblePrimitive } from "radix-ui";
 
+/** @import import { Collapsible } from "@/components/ui/collapsible"; */
 function Collapsible({ ...props }: React.ComponentProps<typeof CollapsiblePrimitive.Root>) {
   return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />;
 }
 
+/** @import import { CollapsibleTrigger } from "@/components/ui/collapsible"; */
 function CollapsibleTrigger({
   ...props
 }: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleTrigger>) {
   return <CollapsiblePrimitive.CollapsibleTrigger data-slot="collapsible-trigger" {...props} />;
 }
 
+/** @import import { CollapsibleContent } from "@/components/ui/collapsible"; */
 function CollapsibleContent({
   ...props
 }: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>) {
